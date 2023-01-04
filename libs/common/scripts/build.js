1de41dcc46037a5ce1aa44f51d90d85287423169
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-01-03 19:38:38 -0800

    chore: add build-script

---
+++ libs/common/scripts/build.js
@@ -0,0 +1,43 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const { writeFile, access, F_OK } = require('fs/promises');
+require('dotenv').config();
+
+/**
+ * This helper script is used to build the library
+ */
+(async () => {
+  try {
+    console.log('>> running custom build script for common library');
+
+    const FILE_PATH = 'src/lib/constants/build-config.ts';
+    const hasFile = access(FILE_PATH, F_OK)
+      .then(() => true)
+      .catch(() => false);
+    if (hasFile) {
+      console.log('>> file already exists, skipping');
+    } else {
+      console.log(
+        '>> build-config file not found, creating from environment variables'
+      );
+      const API_KEY = process.env.API_KEY;
+      if (!API_KEY) {
+        throw new Error('API_KEY env variable not set');
+      }
+      const config = JSON.stringify({
+        API_KEY,
+      });
+      await writeFile(FILE_PATH, config, 'utf8');
+    }
+
+    // **Note** this is the internal nx-focus "build" command
+    await exec('nx run common:_build:production');
+
+    console.log('>> done building common library');
+
+    console.log('>> done!');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
--- libs/common/src/lib/constants/api-key.ts
+++ libs/common/src/lib/constants/api-key.ts
@@ -1,1 +1,5 @@
-export * from '../../../../../build-config';
+/**
+ * This is done for clarity sake, as the `build-config` file isn't kept track of
+ * in source control.
+ */
+export * from './build-config';
