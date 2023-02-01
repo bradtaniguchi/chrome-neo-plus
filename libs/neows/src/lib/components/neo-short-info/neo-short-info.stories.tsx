74216ce50ab02214f6b0ee26164e7d4dee8c60f4
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-01-31 21:53:12 -0800

    feat: add basic neo-short-info

--- libs/neows/src/lib/components/neo-short-info/neo-short-info.stories.tsx
+++ libs/neows/src/lib/components/neo-short-info/neo-short-info.stories.tsx
@@ -1,11 +1,64 @@
 import { Meta, Story } from '@storybook/react';
-import { NeoShortInfo } from './neo-short-info';
+import { withRouter } from 'storybook-addon-react-router-v6';
+import { NeoShortInfo, NeoShortInfoProps } from './neo-short-info';
 
 export default {
   component: NeoShortInfo,
   title: 'components/NeoShortInfo',
+  decorators: [withRouter],
 } as Meta;
 
-const Template: Story = (args) => <NeoShortInfo {...args} />;
+const Template: Story<NeoShortInfoProps> = (args) => <NeoShortInfo {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  neo: {
+    links: {
+      self: 'http://api.nasa.gov/neo/rest/v1/neo/2496816?api_key=ICzn1y9k7kZGjpJb91uaOGxnaKM8zHc7Pnzu2lg5',
+    },
+    id: '2496816',
+    neo_reference_id: '2496816',
+    name: '496816 (1989 UP)',
+    nasa_jpl_url: 'http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2496816',
+    absolute_magnitude_h: 20.56,
+    estimated_diameter: {
+      kilometers: {
+        estimated_diameter_min: 0.2053784995,
+        estimated_diameter_max: 0.459240286,
+      },
+      meters: {
+        estimated_diameter_min: 205.3784995184,
+        estimated_diameter_max: 459.2402860401,
+      },
+      miles: {
+        estimated_diameter_min: 0.1276162436,
+        estimated_diameter_max: 0.2853585958,
+      },
+      feet: {
+        estimated_diameter_min: 673.8139963601,
+        estimated_diameter_max: 1506.6939000519,
+      },
+    },
+    is_potentially_hazardous_asteroid: true,
+    close_approach_data: [
+      {
+        close_approach_date: '2023-01-23',
+        close_approach_date_full: '2023-Jan-23 12:21',
+        epoch_date_close_approach: 1674476460000,
+        relative_velocity: {
+          kilometers_per_second: '16.7129653576',
+          kilometers_per_hour: '60166.6752872232',
+          miles_per_hour: '37385.2322083304',
+        },
+        miss_distance: {
+          astronomical: '0.3255063929',
+          lunar: '126.6219868381',
+          kilometers: '48695063.049223123',
+          miles: '30257709.1101721774',
+        },
+        orbiting_body: 'Earth',
+      },
+    ],
+    is_sentry_object: false,
+  },
+};
--- libs/neows/src/lib/components/neo-short-info/neo-short-info.tsx
+++ libs/neows/src/lib/components/neo-short-info/neo-short-info.tsx
@@ -1,4 +1,27 @@
-import { Card } from 'flowbite-react';
+import {
+  ArrowTopRightOnSquareIcon,
+  BookOpenIcon,
+} from '@heroicons/react/24/solid';
+import { Button, Card } from 'flowbite-react';
+import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
+import { LookupResponse } from '../../models';
+
+export interface NeoShortInfoProps {
+  /**
+   * A single NEO response.
+   */
+  neo: LookupResponse;
+  /**
+   * If the NEO is bookmarked.
+   */
+  isBookmarked?: boolean;
+  /**
+   * Callback that is called when the user clicks on the bookmark.
+   * The value is the new bookmarked state.
+   */
+  bookmarkedChanged?: (bookmarked: boolean) => void;
+}
 
 /**
  * Short info about a single NEO object.
@@ -15,11 +38,86 @@ import { Card } from 'flowbite-react';
  * - link to JPL page
  *
  * Add a "help" for each property.
+ *
+ * @param props the props for the component.
  */
-export function NeoShortInfo() {
+export function NeoShortInfo(props: NeoShortInfoProps) {
+  const { neo, isBookmarked, bookmarkedChanged } = props;
+
+  const passDistance = neo.close_approach_data[0].miss_distance.kilometers;
+
+  /**
+   * Handle the on-bookmark click event.
+   */
+  const handleOnBookmark = () => {
+    if (typeof bookmarkedChanged === 'function')
+      bookmarkedChanged(!isBookmarked);
+  };
+
+  /**
+   * The trimmed-pass-distance is calculated based on the kilometers pass distance
+   * of the next close approach.
+   */
+  const trimmedPassDistance = useMemo(() => {
+    const num = Number(passDistance);
+    if (!isNaN(num)) return Math.round(num);
+    if (typeof num === 'number') return num;
+    return 0;
+  }, [passDistance]);
+
   return (
-    <Card className="dark:bg-slate-800 dark:text-white">
-      <p>info goes here</p>
-    </Card>
+    <Link to={`/neo/${neo.id}`}>
+      <Card className="dark:bg-slate-800 dark:text-white">
+        <div className="flex flex-row justify-between">
+          <h1 className="text-lg">{neo.name}</h1>
+          <div>
+            <Button
+              pill={true}
+              outline={!isBookmarked}
+              onClick={handleOnBookmark}
+            >
+              <BookOpenIcon className="w-4" />
+            </Button>
+          </div>
+        </div>
+        <ul>
+          <li className="flex flex-row justify-between">
+            <div>Neo Ref ID</div>
+            <div>{neo.neo_reference_id}</div>
+          </li>
+          <li className="flex flex-row justify-between">
+            <div>Dangerous</div>
+            {/* Change to chip color? */}
+            <div className="font-bold">
+              {neo.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}
+            </div>
+          </li>
+          <li className="flex flex-row justify-between">
+            <div>Next Pass:</div>
+            <div>{neo.close_approach_data[0].close_approach_date}</div>
+          </li>
+          <li className="flex flex-row justify-between">
+            {/* TODO: change based on global values */}
+            <div>Pass Distance (km)</div>
+            <div title={passDistance.toLocaleString()}>
+              {trimmedPassDistance.toLocaleString()}
+            </div>
+          </li>
+          <li className="flex flex-row justify-end">
+            <a
+              href={neo.nasa_jpl_url}
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-row gap-1"
+            >
+              <ArrowTopRightOnSquareIcon className="w-4" />
+              <div>JPL Link</div>
+            </a>
+          </li>
+
+          {/* TODO: add other data and links to external resources such as JPL */}
+        </ul>
+      </Card>
+    </Link>
   );
 }
