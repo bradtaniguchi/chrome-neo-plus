59b472b33a79b5f4868485a6afd261c7d91fed38
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-02-04 11:04:19 -0800

    chore: move overview component to its own page

--- libs/neows/src/index.ts
+++ libs/neows/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/components';
 export * from './lib/models';
 export * from './lib/utils';
 export * from './lib/hooks';
+export * from './lib/pages/overview';
--- libs/neows/src/lib/components/index.ts
+++ libs/neows/src/lib/components/index.ts
@@ -1,3 +1,2 @@
-export * from './overview';
 export * from './neo-short-info/neo-short-info';
 export * from './best-neo/best-neo';
