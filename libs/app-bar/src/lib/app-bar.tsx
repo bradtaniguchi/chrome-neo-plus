76bb63d5b6b88c003d6f5f8b1bb25d50eecc6aad
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-02-09 17:42:24 -0800

    feat: add app-bar library boilerplate

---
+++ libs/app-bar/src/lib/app-bar.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface AppBarProps {}
+
+export function AppBar(props: AppBarProps) {
+  return (
+    <div>
+      <h1>Welcome to AppBar!</h1>
+    </div>
+  );
+}
+
+export default AppBar;
