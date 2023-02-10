a629f3931b357a635f2e402e2529e04c7fce75ce
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-02-09 18:14:58 -0800

    fix: docs for app-bar

--- libs/app-bar/src/lib/app-bar.tsx
+++ libs/app-bar/src/lib/app-bar.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable-next-line */
 export interface AppBarProps {}
 
+/**
+ * The app-bar is shown at the top of the page.
+ *
+ * @param props The properties of the AppBar component
+ */
 export function AppBar(props: AppBarProps) {
   return (
     <div>
