9a2a8c92feeb19d6d8fc4c11ff35b6ed6f72f067
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-12-22 12:57:18 -0800

    fix: app-bar tests

--- libs/app-bar/src/lib/app-bar.spec.tsx
+++ libs/app-bar/src/lib/app-bar.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
 
 import AppBar from './app-bar';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('AppBar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<AppBar />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <AppBar />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
