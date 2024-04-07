b5daf46ee89abfc0a35815077ed8258a14acd1fe
Brad Taniguchi <bradtaniguchi@gmail.com>
2024-04-06 19:08:37 -0700

    feat: add basic boilerplate for help-page

--- apps/chrome-extension/src/app/pages/help-page.tsx
+++ apps/chrome-extension/src/app/pages/help-page.tsx
@@ -1,14 +1,21 @@
+import { Card } from 'flowbite-react';
+
 /**
- * The help page will show how to use the app.
- *
- * TODO: need to provide the index/definition search along
- * with other general usage information
+ * The help page will show application specific information, and act like
+ * an appendix.
  */
 export default function HelpPage() {
   return (
-    <div>
+    <Card className="dark:bg-slate-800 dark:text-white">
       <h1>Help</h1>
-      <p>This page will show how to use the app.</p>
-    </div>
+      <p>
+        This page provides information on all the information provided on the
+        details page. Consider it an appendix.
+      </p>
+      {/*
+        TODO: add "help-entry" components
+        TODO: add "search" component, using fuzzy
+      */}
+    </Card>
   );
 }
